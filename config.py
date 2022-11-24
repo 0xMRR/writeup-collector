fac9a2a4e28481919503bfb3b66582a0407fa4a3
Mohammadreza Rezaee <104883832+verfosec@users.noreply.github.com>
2022-11-24 13:00:25 +0100

    Initial commit

---
+++ config.py
@@ -0,0 +1,3 @@
+DISCORD_WEBHOOK_URL = ''
+
+DEFAULT_THUMBNAIL_IMAGE_URL = 'https://media.istockphoto.com/vectors/no-thumbnail-image-vector-graphic-vector-id1147544806?k=20&m=1147544806&s=170667a&w=0&h=5rN3TBN7bwbhW_0WyTZ1wU_oW5Xhan2CNd-jlVVnwD0='
